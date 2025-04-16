5f213ebcf2185db02ece5293798b0f3984e0b634
LyNx725 <162893535+LyNx725@users.noreply.github.com>
2025-04-16 11:19:03 +0530

    settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -29,8 +29,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "monitorwildlife@gmail.com"
-EMAIL_HOST_PASSWORD = "jhdfvabdbkcbhobg"
+EMAIL_HOST_USER = "yourmail@example.com"
+EMAIL_HOST_PASSWORD = "password"
 
 
 ALLOWED_HOSTS = []

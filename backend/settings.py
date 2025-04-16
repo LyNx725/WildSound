41156b009a971b62d4a58215c339894ee3250bee
LyNx725 <162893535+LyNx725@users.noreply.github.com>
2025-04-16 11:20:24 +0530

    settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eimi-@eyc-de@8#q%p4w)r%u2pqn8iw8rf^m)^#m0!no-9u%k@'
+SECRET_KEY = 'the secret key when setting up django server'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
